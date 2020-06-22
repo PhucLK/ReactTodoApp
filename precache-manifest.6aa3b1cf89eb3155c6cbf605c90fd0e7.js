17f1febc3a79ecefc488ff9714b78e85a2c102ce
PhucLK <phuclkcarryonto04@gmail.com>
2020-06-22 22:28:38 +0700

    Updates

---
+++ precache-manifest.6aa3b1cf89eb3155c6cbf605c90fd0e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "080ab6a059c4aaa8a348abf168d5b21d",
+    "url": "/ReactTodoApp/index.html"
+  },
+  {
+    "revision": "bbfe6c7f5bf7bf8e4fdd",
+    "url": "/ReactTodoApp/static/css/main.b74948f6.chunk.css"
+  },
+  {
+    "revision": "93815cf875d3967f08d6",
+    "url": "/ReactTodoApp/static/js/2.170bd7b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactTodoApp/static/js/2.170bd7b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbfe6c7f5bf7bf8e4fdd",
+    "url": "/ReactTodoApp/static/js/main.1e414796.chunk.js"
+  },
+  {
+    "revision": "3c12ae128700dc47e615",
+    "url": "/ReactTodoApp/static/js/runtime-main.6b1569aa.js"
+  }
+]);
